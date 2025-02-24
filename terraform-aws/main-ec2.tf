c2e6cd91b2a30424c6d8885a8864e80aa85d03c2
Ariel-ksenzovsky <a.ksenzovsky@gmail.com>
2025-02-24 15:49:41 +0200

    test to see if the cicd workflow is working no.3

--- terraform-aws/main-ec2.tf
+++ terraform-aws/main-ec2.tf
@@ -74,10 +74,9 @@ resource "aws_instance" "docker_instance" {
               sudo yum install -y git ansible
 
               # Clone the repository containing the Ansible playbook
-              git clone https://github.com/Ariel-ksenzovsky/star-image-app.git /home/ec2-user/star-image-app
-
+              git clone AWS-Terraform-Instance /home/ec2-user/AWS-Terraform-Instance
               # Run the Ansible playbook
-              ansible-playbook /home/ec2-user/star-image-app/ansible-flask-app-ec2.yml
+              ansible-playbook /home/ec2-user/AWS-Terraform-Instance/ansible-flask-app-ec2.yml
               EOF
 
   tags = {
